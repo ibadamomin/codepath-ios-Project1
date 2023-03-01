89e03242fc74a9b783d71adea5d223cfa45c8922
Ibad Momin <ibadalimomin@gmail.com>
2023-03-01 02:12:26 -0600

    added app icon

--- Project1/Project1/fourthViewController.swift
+++ Project1/Project1/fourthViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class fourthViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
